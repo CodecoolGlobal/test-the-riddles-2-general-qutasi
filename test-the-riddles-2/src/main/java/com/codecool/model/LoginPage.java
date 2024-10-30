1c5007dabce01c12b2409534a3c9f741601dfc1a
faboknorbert <fabok.norbert94@gmail.com>
2024-10-30 11:25:45 +0100

    Extend LoginPage with Navbar component

--- test-the-riddles-2/src/main/java/com/codecool/model/LoginPage.java
+++ test-the-riddles-2/src/main/java/com/codecool/model/LoginPage.java
@@ -3,16 +3,14 @@ package com.codecool.model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage {
+public class LoginPage extends NavbarPage {
     public static final String URL = "http://localhost:3000/login";
-    
-    protected WebDriver driver;
     private By userNameBy = By.id("user-name");
     private By passwordBy = By.id("password");
     private By loginButtonBy = By.xpath("//button[contains(text(),'LOGIN')]");
     
     public LoginPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
     
     public HomePage login(String username, String password) {
@@ -21,6 +19,5 @@ public class LoginPage {
         driver.findElement(loginButtonBy).click();
         return new HomePage(driver);
     }
-    
     
 }
