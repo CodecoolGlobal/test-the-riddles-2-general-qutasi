ebb839df49b0c8eae0bed9b53273bf2c28bae026
faboknorbert <fabok.norbert94@gmail.com>
2024-10-30 11:25:19 +0100

    Extend HomePage with Navbar component

--- test-the-riddles-2/src/main/java/com/codecool/model/HomePage.java
+++ test-the-riddles-2/src/main/java/com/codecool/model/HomePage.java
@@ -3,13 +3,13 @@ package com.codecool.model;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class HomePage {
+public class HomePage extends NavbarPage{
     
     protected WebDriver driver;
     public static final String URL = "http://localhost:3000/";
     
     public HomePage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
     
 }
