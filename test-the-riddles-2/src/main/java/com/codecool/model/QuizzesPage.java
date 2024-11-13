4f8beb2ae73a39bff85bde0cd564f7a785f351c1
faboknorbert <fabok.norbert94@gmail.com>
2024-11-13 14:22:50 +0100

    refactor QuizzesPage

--- test-the-riddles-2/src/main/java/com/codecool/model/QuizzesPage.java
+++ test-the-riddles-2/src/main/java/com/codecool/model/QuizzesPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebDriver;
 
 public class QuizzesPage extends NavbarPage{
 
-    private final By firstPlayBtnBy = By.cssSelector("button.bg-green-400:nth-child(3)");
-    private final By firstCopyBtnBy = By.cssSelector(".bg-yellow-400");
-    private final By addQuizBtnBy = By.cssSelector(".w-32");
+    private final By firstPlayBtnBy = By.xpath("//button[contains(text(),'Play')]");
+    private final By firstCopyBtnBy = By.xpath("//button[contains(text(),'Copy')]");
+    private final By addQuizBtnBy = By.xpath("//button[contains(text(),'Add Quiz')]");
     public QuizzesPage(WebDriver driver) {
         super(driver);
     }
