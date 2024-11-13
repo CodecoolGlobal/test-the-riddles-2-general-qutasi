380abceaf0c1b77e45f13e23cffcffa233c88e01
faboknorbert <fabok.norbert94@gmail.com>
2024-11-13 20:23:12 +0100

    removed unused imports

--- test-the-riddles-2/src/test/java/com/codecool/model/LoginPageTest.java
+++ test-the-riddles-2/src/test/java/com/codecool/model/LoginPageTest.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginPageTest {
