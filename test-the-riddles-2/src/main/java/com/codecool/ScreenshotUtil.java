595a08189d877d671b5f963ad99c8e0b9c2c6d75
kuta <150553585+qutasi@users.noreply.github.com>
2024-11-14 14:10:00 +0100

    add screenshot feature to testKeepLoginOnAccount method in LoginPageTest

--- test-the-riddles-2/src/main/java/com/codecool/ScreenshotUtil.java
+++ test-the-riddles-2/src/main/java/com/codecool/ScreenshotUtil.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -16,13 +17,14 @@ public class ScreenshotUtil {
     public static void takeScreenshot(WebDriver driver, String testName) {
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-        String fileName = "screenshots" + testName + "//" + timestamp + ".png";
+        Path screenshotsDir = Paths.get("screenshots");
+        Path filePath = screenshotsDir.resolve(testName + "_" + timestamp + ".png");
         try {
-            Files.createDirectories(Paths.get("screenshots"));
-            Files.copy(screenshot.toPath(), Paths.get(fileName));
-            System.out.println("Screenshot saved: " + fileName);
+            Files.createDirectories(screenshotsDir);
+            Files.copy(screenshot.toPath(), filePath);
+            System.out.println("Screenshot saved: " + filePath);
         } catch (IOException e) {
-            System.out.println("Screenshot could not be saved: " + fileName);
+            System.out.println("Screenshot could not be saved");
         }
     }
 }
